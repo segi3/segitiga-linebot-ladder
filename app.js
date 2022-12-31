59cd5e81a6525da2fb5522ce49f1c16fc15e7ad4
Rafi Nizar Abiyyi <rafi.nizar15@gmail.com>
2022-12-31 22:42:05 +0700

    debug line

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const config = {
     channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
     channelSecret: process.env.LINE_CHANNEL_SECRET
 }
-
+console.log(config)
 const app = require('express')()
 const line = require('@line/bot-sdk')
 
