70db4d9c9dce1ec668209ec1099cf3ff171f54e7
Rafi Nizar Abiyyi <rafi.nizar15@gmail.com>
2022-12-31 23:23:15 +0700

    beautify

--- app.js
+++ app.js
@@ -7,31 +7,34 @@ const config = {
 const app = require('express')()
 const line = require('@line/bot-sdk')
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.status(200).send('Chatbot Tutorial')
 })
 
 app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result))
-      .catch((err) => {
-        console.error(err)
-        console.error(err.originalError.response.data)
-        console.error(err.response.data)
-        res.status(500).end()
-      })
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err)
+            console.error(err.originalError.response.data)
+            console.error(err.response.data)
+            res.status(500).end()
+        })
 })
 
 const client = new line.Client(config)
 const handleEvent = (event) => {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null)
-  }
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null)
+    }
 
-  const echo = { type: 'text', text: event.message.text }
+    const echo = {
+        type: 'text',
+        text: event.message.text
+    }
 
-  return client.replyMessage(event.replyToken, echo)
+    return client.replyMessage(event.replyToken, echo)
 }
 
 app.listen(process.env.PORT)
