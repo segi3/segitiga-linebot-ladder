47245d3446a1416af80e5ed761b18833f99dffd8
Rafi Nizar Abiyyi <rafi.nizar15@gmail.com>
2023-01-01 10:36:27 +0700

    catch any error

--- app.js
+++ app.js
@@ -31,127 +31,133 @@ const handleEvent = (event) => {
         return Promise.resolve(null)
     }
 
-    if (event.message.text.toLowerCase().includes('/ladder')) {
-        const groupId = event.source.groupId
-        const raw = event.message.text
-        const filtered = raw.trim().replace(/\n/g, ' ')
-
-        const opsiRegex = /(?<=[|]).+?(?=[\|@])/g
-        const orangRegex = /(?<=@).+?(?=@|$)/g
-
-        const opsiArr = filtered.match(opsiRegex).map(s => s.trim())
-        const orangArr = filtered.match(orangRegex).map(s => s.trim())
-
-        let txtReply = ''
-
-        if (opsiArr.length != orangArr.length) {
-            txtReply = 'Jumlah opsi sama orangnya gak sama'
-            return client.replyMessage(event.replyToken, {
+    try {
+        if (event.message.text.toLowerCase().includes('/ladder')) {
+            const groupId = event.source.groupId
+            const raw = event.message.text
+            const filtered = raw.trim().replace(/\n/g, ' ')
+    
+            const opsiRegex = /(?<=[|]).+?(?=[\|@])/g
+            const orangRegex = /(?<=@).+?(?=@|$)/g
+    
+            const opsiArr = filtered.match(opsiRegex).map(s => s.trim())
+            const orangArr = filtered.match(orangRegex).map(s => s.trim())
+    
+            let txtReply = ''
+    
+            if (opsiArr.length != orangArr.length) {
+                txtReply = 'Jumlah opsi sama orangnya gak sama'
+                return client.replyMessage(event.replyToken, {
+                    type: 'text',
+                    text: txtReply
+                })
+            }
+    
+            txtReply = 'Opsi\n'
+            for (let op in opsiArr) {
+                txtReply = txtReply + `- ${opsiArr[op]}\n`
+            }
+            txtReply = txtReply + 'Orang\n'
+            for (let or in orangArr) {
+                txtReply = txtReply + `- ${orangArr[or]}\n`
+            }
+    
+            let shuffled = orangArr
+                .map(value => ({
+                    value,
+                    sort: Math.random()
+                }))
+                .sort((a, b) => a.sort - b.sort)
+                .map(({
+                    value
+                }) => value)
+    
+            txtReply = txtReply + 'Hasil Ladder\n'
+            for (let i = 0; i < opsiArr.length; i++) {
+                txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}`
+                if (i+1 != opsiArr.length) {
+                    txtReply+=`\n`
+                }
+            }
+    
+            const replyObj = {
                 type: 'text',
                 text: txtReply
-            })
-        }
-
-        txtReply = 'Opsi\n'
-        for (let op in opsiArr) {
-            txtReply = txtReply + `- ${opsiArr[op]}\n`
-        }
-        txtReply = txtReply + 'Orang\n'
-        for (let or in orangArr) {
-            txtReply = txtReply + `- ${orangArr[or]}\n`
-        }
-
-        let shuffled = orangArr
-            .map(value => ({
-                value,
-                sort: Math.random()
-            }))
-            .sort((a, b) => a.sort - b.sort)
-            .map(({
-                value
-            }) => value)
-
-        txtReply = txtReply + 'Hasil Ladder\n'
-        for (let i = 0; i < opsiArr.length; i++) {
-            txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}`
-            if (i+1 != opsiArr.length) {
-                txtReply+=`\n`
             }
-        }
-
-        const replyObj = {
-            type: 'text',
-            text: txtReply
-        }
-
-        let toSaveToCache = {
-            opsiArr,
-            orangArr
-        }
-        cache[groupId] = toSaveToCache
-
-        console.log(`saved ${groupId} to cache, rn ${cache}`)
-        console.log(cache)
-
-        return client.replyMessage(event.replyToken, replyObj)
-
-    } else if (event.message.text.toLowerCase().includes('/reshuffle')) {
-        const groupId = event.source.groupId
-
-        console.log(`searching for ${groupId} in ${cache}`)
-        console.log(cache)
-
-        let txtReply = ''
-        if (cache[groupId] === undefined || cache[groupId] === null) {
-            txtReply = 'Gak bisa reshuffle, belum ada data ladder :('
-            return client.replyMessage(event.replyToken, {
+    
+            let toSaveToCache = {
+                opsiArr,
+                orangArr
+            }
+            cache[groupId] = toSaveToCache
+    
+            console.log(`saved ${groupId} to cache, rn ${cache}`)
+            console.log(cache)
+    
+            return client.replyMessage(event.replyToken, replyObj)
+    
+        } else if (event.message.text.toLowerCase().includes('/reshuffle')) {
+            const groupId = event.source.groupId
+    
+            console.log(`searching for ${groupId} in ${cache}`)
+            console.log(cache)
+    
+            let txtReply = ''
+            if (cache[groupId] === undefined || cache[groupId] === null) {
+                txtReply = 'Gak bisa reshuffle, belum ada data ladder :('
+                return client.replyMessage(event.replyToken, {
+                    type: 'text',
+                    text: txtReply
+                })
+            }
+            const opsiArr = cache[groupId].opsiArr
+            const orangArr = cache[groupId].orangArr
+    
+            let shuffled = orangArr
+                .map(value => ({
+                    value,
+                    sort: Math.random()
+                }))
+                .sort((a, b) => a.sort - b.sort)
+                .map(({
+                    value
+                }) => value)
+    
+            txtReply = 'Hasil Ladder\n'
+            for (let i = 0; i < opsiArr.length; i++) {
+                txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}`
+                if (i+1 != opsiArr.length) {
+                    txtReply+=`\n`
+                }
+            }
+    
+            const replyObj = {
                 type: 'text',
                 text: txtReply
+            }
+    
+            return client.replyMessage(event.replyToken, replyObj)
+    
+        } else if (event.message.text.toLowerCase().includes('/bangun')) {
+            return client.replyMessage(event.replyToken, {
+                type: 'text',
+                text: 'siap udah bangun'
+            })
+        } else if (event.message.text.toLowerCase().includes('/dbcache')) {
+            console.log(cache)
+        } else if (event.message.text.toLowerCase().includes('/groupid')) {
+            console.log(`groupId => ${event.source.groupId}`)
+            return client.replyMessage(event.replyToken, {
+                type: 'text',
+                text: event.source.groupId
             })
         }
-        const opsiArr = cache[groupId].opsiArr
-        const orangArr = cache[groupId].orangArr
-
-        let shuffled = orangArr
-            .map(value => ({
-                value,
-                sort: Math.random()
-            }))
-            .sort((a, b) => a.sort - b.sort)
-            .map(({
-                value
-            }) => value)
-
-        txtReply = 'Hasil Ladder\n'
-        for (let i = 0; i < opsiArr.length; i++) {
-            txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}`
-            if (i+1 != opsiArr.length) {
-                txtReply+=`\n`
-            }
-        }
-
-        const replyObj = {
+    } catch (e) {
+        return client.replyMessage(event.replyToken, {
             type: 'text',
-            text: txtReply
-        }
-
-        return client.replyMessage(event.replyToken, replyObj)
-
-    } else if (event.message.text.toLowerCase().includes('/bangun')) {
-        return client.replyMessage(event.replyToken, {
-            type: 'siap udah bangun',
-            text: txtReply
-        })
-    } else if (event.message.text.toLowerCase().includes('/dbcache')) {
-        console.log(cache)
-    } else if (event.message.text.toLowerCase().includes('/groupid')) {
-        console.log(`groupId => ${event.source.groupId}`)
-        return client.replyMessage(event.replyToken, {
-            type: event.source.groupId,
-            text: txtReply
+            text: 'command gak bisa di proses, ada error di app :('
         })
     }
-
 }
 
 app.listen(process.env.PORT)
