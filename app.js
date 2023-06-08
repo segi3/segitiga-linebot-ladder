debf54c3b99b71fe42dcd8163d2b3b7a72103f35
Rafi Nizar A <adl.rafiniza@xl.co.id>
2023-06-08 16:41:14 +0700

    INITIATE REFACTOR AAAAAAAAAAAAAAA

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const config = {
 }
 
 let cache = {}
-const Redis = require("ioredis")
-const { REDIS_URL } = process.env
-const renderRedis = new Redis(REDIS_URL)
+let renderRedis
+try {
+    const Redis = require("ioredis")
+    const { REDIS_URL } = process.env
+    renderRedis = new Redis(REDIS_URL)
+    console.log('connected to render')
+} catch (err) {
+    console.log(err)
+}
+
 
 // load config from mongodb, not yet used as db
 const { MongoClient } = require("mongodb")
@@ -19,6 +26,7 @@ const initMongodbAndConfig = async () => {
     
     const appConfig = mongodb.collection('config')
     whitelist = await appConfig.findOne({ type: 'whitelist' })
+    console.log('connected to mongo')
 }
 initMongodbAndConfig()
 
@@ -42,9 +50,21 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 })
 
 const client = new line.Client(config)
+const {ladderHandler} = require('./internal/ladder/handler/handler')
+
 const handleEvent = async (event) => {
     if (event.type !== 'message' || event.message.type !== 'text') {
         return Promise.resolve(null)
+    }
+
+    try {
+        ladderHandler(event, client, renderRedis)
+    } catch (e) {
+        console.log(e)
+        return client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: 'nanti ya lagi sibuk :('
+        })
     }
 
     try {
@@ -58,116 +78,118 @@ const handleEvent = async (event) => {
         // }
 
         // text message event
-        if (event.message.text.toLowerCase().includes('/ladder')) {
-            const groupId = event.source.groupId ? event.source.groupId : event.source.roomId
-            const raw = event.message.text
-            const filtered = raw.trim().replace(/\n/g, ' ')
+        // if (event.message.text.toLowerCase().includes('/ladder')) {
+        //     const groupId = event.source.groupId ? event.source.groupId : event.source.roomId
+        //     const raw = event.message.text
+        //     const filtered = raw.trim().replace(/\n/g, ' ')
     
-            const opsiRegex = /(?<=[|]).+?(?=[\|@])/g
-            const orangRegex = /(?<=@).+?(?=@|$)/g
+        //     const opsiRegex = /(?<=[|]).+?(?=[\|@])/g
+        //     const orangRegex = /(?<=@).+?(?=@|$)/g
     
-            const opsiArr = filtered.match(opsiRegex).map(s => s.trim())
-            const orangArr = filtered.match(orangRegex).map(s => s.trim())
+        //     const opsiArr = filtered.match(opsiRegex).map(s => s.trim())
+        //     const orangArr = filtered.match(orangRegex).map(s => s.trim())
     
-            let txtReply = ''
+        //     let txtReply = ''
     
-            if (opsiArr.length != orangArr.length) {
-                txtReply = 'Jumlah opsi sama orangnya gak sama'
-                return client.replyMessage(event.replyToken, {
-                    type: 'text',
-                    text: txtReply
-                })
-            }
+        //     if (opsiArr.length != orangArr.length) {
+        //         txtReply = 'Jumlah opsi sama orangnya gak sama'
+        //         return client.replyMessage(event.replyToken, {
+        //             type: 'text',
+        //             text: txtReply
+        //         })
+        //     }
     
-            let shuffled = orangArr
-                .map(value => ({
-                    value,
-                    sort: Math.random()
-                }))
-                .sort((a, b) => a.sort - b.sort)
-                .map(({
-                    value
-                }) => value)
+        //     let shuffled = orangArr
+        //         .map(value => ({
+        //             value,
+        //             sort: Math.random()
+        //         }))
+        //         .sort((a, b) => a.sort - b.sort)
+        //         .map(({
+        //             value
+        //         }) => value)
     
-            txtReply = '*Hasil Ladder*\n'
-            let reShuffleMsg = '\nDalam 15 menit, hasil ladder bisa di reshuffle pakek `/reshuffle`'
-            for (let i = 0; i < opsiArr.length; i++) {
-                txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}\n`
-            }
+        //     txtReply = '*Hasil Ladder*\n'
+        //     let reShuffleMsg = '\nDalam 15 menit, hasil ladder bisa di reshuffle pakek `/reshuffle`'
+        //     for (let i = 0; i < opsiArr.length; i++) {
+        //         txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}\n`
+        //     }
     
-            const replyObj = {
-                type: 'text',
-                text: txtReply + reShuffleMsg
-            }
+        //     const replyObj = {
+        //         type: 'text',
+        //         text: txtReply + reShuffleMsg
+        //     }
     
-            let toSaveToCache = {
-                opsiArr,
-                orangArr
-            }
+        //     let toSaveToCache = {
+        //         opsiArr,
+        //         orangArr
+        //     }
 
-            // cache[groupId] = toSaveToCache
-            console.log(`saved ${groupId} to cache, rn ${cache}`)
-            // console.log(cache)
+        //     // cache[groupId] = toSaveToCache
+        //     console.log(`saved ${groupId} to cache, rn ${cache}`)
+        //     // console.log(cache)
 
-            renderRedis.set(groupId, JSON.stringify(toSaveToCache), 'EX', 900) // set to expire in 15 minutes
+        //     renderRedis.set(groupId, JSON.stringify(toSaveToCache), 'EX', 900) // set to expire in 15 minutes
     
-            return client.replyMessage(event.replyToken, replyObj)
+        //     return client.replyMessage(event.replyToken, replyObj)
     
-        } else if (event.message.text.toLowerCase().includes('/reshuffle')) {
-            const groupId = event.source.groupId ? event.source.groupId : event.source.roomId
-            let txtReply = ''
+        // } else if (event.message.text.toLowerCase().includes('/reshuffle')) {
+        //     const groupId = event.source.groupId ? event.source.groupId : event.source.roomId
+        //     let txtReply = ''
 
-            /* cache using json */
-            console.log(`searching for ${groupId} in ${cache}`)
-            // console.log(cache)
-            // if (cache[groupId] === undefined || cache[groupId] === null) {
-            //     txtReply = 'Gak bisa reshuffle, belum ada data ladder :('
-            //     return client.replyMessage(event.replyToken, {
-            //         type: 'text',
-            //         text: txtReply
-            //     })
-            // }
-            // const opsiArr = cache[groupId].opsiArr
-            // const orangArr = cache[groupId].orangArr
+        //     /* cache using json */
+        //     console.log(`searching for ${groupId} in ${cache}`)
+        //     // console.log(cache)
+        //     // if (cache[groupId] === undefined || cache[groupId] === null) {
+        //     //     txtReply = 'Gak bisa reshuffle, belum ada data ladder :('
+        //     //     return client.replyMessage(event.replyToken, {
+        //     //         type: 'text',
+        //     //         text: txtReply
+        //     //     })
+        //     // }
+        //     // const opsiArr = cache[groupId].opsiArr
+        //     // const orangArr = cache[groupId].orangArr
 
-            /* cache using redis */
-            let cached = await renderRedis.get(groupId)
-            if (cached === null) {
-                txtReply = 'Gak bisa reshuffle, belum ada data ladder :('
-                return client.replyMessage(event.replyToken, {
-                    type: 'text',
-                    text: txtReply
-                })
-            }
-            cached = JSON.parse(cached)
-            const { opsiArr, orangArr } = cached
+        //     /* cache using redis */
+        //     let cached = await renderRedis.get(groupId)
+        //     if (cached === null) {
+        //         txtReply = 'Gak bisa reshuffle, belum ada data ladder :('
+        //         return client.replyMessage(event.replyToken, {
+        //             type: 'text',
+        //             text: txtReply
+        //         })
+        //     }
+        //     cached = JSON.parse(cached)
+        //     const { opsiArr, orangArr } = cached
     
-            let shuffled = orangArr
-                .map(value => ({
-                    value,
-                    sort: Math.random()
-                }))
-                .sort((a, b) => a.sort - b.sort)
-                .map(({
-                    value
-                }) => value)
+        //     let shuffled = orangArr
+        //         .map(value => ({
+        //             value,
+        //             sort: Math.random()
+        //         }))
+        //         .sort((a, b) => a.sort - b.sort)
+        //         .map(({
+        //             value
+        //         }) => value)
     
-            txtReply = '*Reshuffle Ladder*\n'
-            for (let i = 0; i < opsiArr.length; i++) {
-                txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}`
-                if (i+1 != opsiArr.length) {
-                    txtReply+=`\n`
-                }
-            }
+        //     txtReply = '*Reshuffle Ladder*\n'
+        //     for (let i = 0; i < opsiArr.length; i++) {
+        //         txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}`
+        //         if (i+1 != opsiArr.length) {
+        //             txtReply+=`\n`
+        //         }
+        //     }
     
-            const replyObj = {
-                type: 'text',
-                text: txtReply
-            }
+        //     const replyObj = {
+        //         type: 'text',
+        //         text: txtReply
+        //     }
     
-            return client.replyMessage(event.replyToken, replyObj)
+        //     return client.replyMessage(event.replyToken, replyObj)
     
-        } else if (event.message.text.toLowerCase().includes('/bantu') || 
+        // }
+        
+        if (event.message.text.toLowerCase().includes('/bantu') || 
                     event.message.text.toLowerCase().includes('/tolong') ||
                     event.message.text.toLowerCase().includes('/help')) {
             const helpMsg = `Cara pakai Ladder Bot:\n\nAwali message dengan \`/ladder\`\n\nSetiap opsi di tulis setelah command, dan setiap kalimat opsi diawali dengan simbol \`|\`\n\nPartisipan ditulis setelah opsi, dan setiap nama partisipan diawali dengan simbol \`@\`\n\ncontoh message:\n\n/ladder\n| opsi satu\n| opsi dua\n| opsi tiga\n@ kamu @ aku @juga dia\n`
@@ -193,32 +215,6 @@ const handleEvent = async (event) => {
             return client.replyMessage(event.replyToken, {
                 type: 'text',
                 text: 'id: ' + id
-            })
-        } else if (event.message.text.toLowerCase().includes('/health')) {
-            const userId = event.source.userId ? event.source.userId : null
-            if (userId === null || whitelist.users.includes(userId)) {
-                return client.replyMessage(event.replyToken, {
-                    type: 'text',
-                    text: 'nope'
-                })
-            }
-
-            const months = [
-                'Mei', 'Juni',
-                ]
-                
-            const day = [
-                'Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum\'at', 'Sabtu', 'Minggu']
-
-            let offset = '+7.0'
-            var d = new Date()
-            var utc = d.getTime() + (d.getTimezoneOffset() * 60000)
-            var nd = new Date(utc + (3600000*offset))
-
-            let str = `Absen Pagi ${day[nd.getDay()]} ${nd.getDate()} ${months[nd.getMonth()]} ${nd.getFullYear()}\nNama: Rafi Nizar A\nKondisi: Sehat`
-            return client.replyMessage(event.replyToken, {
-                type: 'text',
-                text: str
             })
         }
     } catch (e) {
---
+++ internal/ladder/handler/handler.js
@@ -0,0 +1,114 @@
+const ladderHandler = async (client, event, renderRedis) => {
+    // text message event
+    if (event.message.text.toLowerCase().includes('/ladder')) {
+        const groupId = event.source.groupId ? event.source.groupId : event.source.roomId
+        const raw = event.message.text
+        const filtered = raw.trim().replace(/\n/g, ' ')
+
+        const opsiRegex = /(?<=[|]).+?(?=[\|@])/g
+        const orangRegex = /(?<=@).+?(?=@|$)/g
+
+        const opsiArr = filtered.match(opsiRegex).map(s => s.trim())
+        const orangArr = filtered.match(orangRegex).map(s => s.trim())
+
+        let txtReply = ''
+
+        if (opsiArr.length != orangArr.length) {
+            txtReply = 'Jumlah opsi sama orangnya gak sama'
+            return client.replyMessage(event.replyToken, {
+                type: 'text',
+                text: txtReply
+            })
+        }
+
+        let shuffled = orangArr
+            .map(value => ({
+                value,
+                sort: Math.random()
+            }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({
+                value
+            }) => value)
+
+        txtReply = '*Hasil Ladder*\n'
+        let reShuffleMsg = '\nDalam 15 menit, hasil ladder bisa di reshuffle pakek `/reshuffle`'
+        for (let i = 0; i < opsiArr.length; i++) {
+            txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}\n`
+        }
+
+        const replyObj = {
+            type: 'text',
+            text: txtReply + reShuffleMsg
+        }
+
+        let toSaveToCache = {
+            opsiArr,
+            orangArr
+        }
+
+        // cache[groupId] = toSaveToCache
+        console.log(`saved ${groupId} to cache, rn ${cache}`)
+        // console.log(cache)
+
+        renderRedis.set(groupId, JSON.stringify(toSaveToCache), 'EX', 900) // set to expire in 15 minutes
+
+        return client.replyMessage(event.replyToken, replyObj)
+
+    } else if (event.message.text.toLowerCase().includes('/reshuffle')) {
+        const groupId = event.source.groupId ? event.source.groupId : event.source.roomId
+        let txtReply = ''
+
+        /* cache using json */
+        console.log(`searching for ${groupId} in ${cache}`)
+        // console.log(cache)
+        // if (cache[groupId] === undefined || cache[groupId] === null) {
+        //     txtReply = 'Gak bisa reshuffle, belum ada data ladder :('
+        //     return client.replyMessage(event.replyToken, {
+        //         type: 'text',
+        //         text: txtReply
+        //     })
+        // }
+        // const opsiArr = cache[groupId].opsiArr
+        // const orangArr = cache[groupId].orangArr
+
+        /* cache using redis */
+        let cached = await renderRedis.get(groupId)
+        if (cached === null) {
+            txtReply = 'Gak bisa reshuffle, belum ada data ladder :('
+            return client.replyMessage(event.replyToken, {
+                type: 'text',
+                text: txtReply
+            })
+        }
+        cached = JSON.parse(cached)
+        const { opsiArr, orangArr } = cached
+
+        let shuffled = orangArr
+            .map(value => ({
+                value,
+                sort: Math.random()
+            }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({
+                value
+            }) => value)
+
+        txtReply = '*Reshuffle Ladder*\n'
+        for (let i = 0; i < opsiArr.length; i++) {
+            txtReply = txtReply + `- ${opsiArr[i]} > ${shuffled[i]}`
+            if (i+1 != opsiArr.length) {
+                txtReply+=`\n`
+            }
+        }
+
+        const replyObj = {
+            type: 'text',
+            text: txtReply
+        }
+
+        return client.replyMessage(event.replyToken, replyObj)
+    }
+}
+
+module.exports = { ladderHandler }
